205b13d0ad79f800eecfc1ef344aa2dbff6216d8
justYu2001 <jack4781012@gmail.com>
2023-06-16 13:33:21 +0800

    feat: sign in with MetaMask

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,38 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import type { AppType } from "next/app";
+
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { hardhat, mainnet } from "wagmi/chains";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { publicProvider } from "wagmi/providers/public";
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
+const { chains, publicClient } = configureChains(
+    [mainnet, hardhat],
+    [publicProvider()]
+);
+
+const config = createConfig({
+    autoConnect: true,
+    connectors: [new MetaMaskConnector({ chains })],
+    publicClient,
+});
+
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+    Component,
+    pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+    return (
+        <WagmiConfig config={config}>
+            <SessionProvider session={session} refetchInterval={0}>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </WagmiConfig>
+    );
 };
 
 export default api.withTRPC(MyApp);
